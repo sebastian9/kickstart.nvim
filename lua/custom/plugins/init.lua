25c5f4b16be8826a7e910424c3a8d6726b78c332
Sebastian Lopez Sanchez <slopezsanchez@tesla.com>
2024-07-16 15:47:00 -0700

    Install nerdtree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,8 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.keymap.set('n', '<leader>n', ':NERDTreeFocus<CR>', { desc = 'Open [N]ERDTreeFocus' })
+
 return {
   'github/copilot.vim',
   'ThePrimeagen/vim-be-good',
   'ryanoasis/vim-devicons',
+  'preservim/nerdtree',
 }
